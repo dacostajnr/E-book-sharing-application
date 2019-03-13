01bc94a48c06a5903ce87d326f198715b6dbf1b9
slimtrissly <slimtrissly@gmail.com>
2019-03-13 22:17:01 +1200

    First Commit

--- bookApp/urls.py
+++ bookApp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url,include
 from django.contrib import admin
 from book import views
+
 from django.conf.urls.static import  static 
 from django.conf import settings 
 from django.views.static import serve
